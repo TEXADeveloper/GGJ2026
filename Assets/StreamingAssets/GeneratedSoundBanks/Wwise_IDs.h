ee1de73261ffad177a59bb1ec424a42d35b6d06d
Juan Mariño <juan.marino03@gmail.com>
2026-02-01 12:01:47 -0300

    Audio: Added footsteps, ghost voices and regenerated SoundBanks

--- Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,14 +13,31 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_GHOSTBREATH = 4037826725U;
         static const AkUniqueID PLAY_GHOSTHOVER = 7578887U;
+        static const AkUniqueID PLAY_GHOSTVOICECATCHPLAYER = 3959760815U;
+        static const AkUniqueID PLAY_GHOSTVOICELINE = 4150122441U;
         static const AkUniqueID PLAY_MAINMUSIC = 1346240278U;
         static const AkUniqueID PLAY_PLAYERFOOTSTEPS = 1805011878U;
+        static const AkUniqueID SET_MUSICVOLUMESLIDER = 2284819954U;
+        static const AkUniqueID SET_SFXVOLUMESLIDER = 1396280654U;
         static const AkUniqueID STOP_MAINMUSIC = 665673276U;
     } // namespace EVENTS
 
     namespace SWITCHES
     {
+        namespace DISTANCE
+        {
+            static const AkUniqueID GROUP = 1240670792U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAR = 1183803292U;
+                static const AkUniqueID MID = 1182670505U;
+                static const AkUniqueID NEAR = 1133794587U;
+            } // namespace SWITCH
+        } // namespace DISTANCE
+
         namespace SURFACE
         {
             static const AkUniqueID GROUP = 1834394558U;
@@ -37,6 +54,8 @@ namespace AK
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID DISTANCE = 1240670792U;
+        static const AkUniqueID MUSIC_VOLUMESLIDER = 3415784672U;
+        static const AkUniqueID SFX_VOLUMESLIDER = 250326712U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
         static const AkUniqueID SS_AIR_FURY = 1029930033U;
