066a3d630d06cc17fb87f41244a45b3989dcddeb
Juan Mariño <juan.marino03@gmail.com>
2026-02-01 12:58:36 -0300

    Audio: Regenerated Soundbanks

---
+++ Assets/StreamingAssets/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,103 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_GHOSTBREATH = 4037826725U;
+        static const AkUniqueID PLAY_GHOSTHOVER = 7578887U;
+        static const AkUniqueID PLAY_GHOSTVOICECATCHPLAYER = 3959760815U;
+        static const AkUniqueID PLAY_GHOSTVOICELINE = 4150122441U;
+        static const AkUniqueID PLAY_MAINMENU_BUTTONCLICK = 2940935649U;
+        static const AkUniqueID PLAY_MAINMENU_BUTTONHOVER = 963961931U;
+        static const AkUniqueID PLAY_MAINMUSIC = 1346240278U;
+        static const AkUniqueID PLAY_PLAYERFLASHLIGHTTURNOFF = 283418385U;
+        static const AkUniqueID PLAY_PLAYERFLASHLIGHTTURNON = 3293325573U;
+        static const AkUniqueID PLAY_PLAYERFOOTSTEPS = 1805011878U;
+        static const AkUniqueID PLAY_PLAYERGRABOBJECT = 1807050870U;
+        static const AkUniqueID PLAY_PLAYERMASKPUTON = 1925692715U;
+        static const AkUniqueID SET_MUSICVOLUMESLIDER = 2284819954U;
+        static const AkUniqueID SET_SFXVOLUMESLIDER = 1396280654U;
+        static const AkUniqueID STOP_MAINMUSIC = 665673276U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace DISTANCE
+        {
+            static const AkUniqueID GROUP = 1240670792U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAR = 1183803292U;
+                static const AkUniqueID MID = 1182670505U;
+                static const AkUniqueID NEAR = 1133794587U;
+            } // namespace SWITCH
+        } // namespace DISTANCE
+
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID CONCRETE = 841620460U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCE = 1240670792U;
+        static const AkUniqueID MUSIC_VOLUMESLIDER = 3415784672U;
+        static const AkUniqueID SFX_VOLUMESLIDER = 250326712U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ROOMREVERB = 1340358820U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
